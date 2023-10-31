3d673842a1f400e6878a78160a71caaf400773f9
Benjamin MacLellan <bmaclell@uwaterloo.ca>
2023-10-31 13:24:36 -0400

    Demo of Hamiltonian layer with Pydantic/FastAPI

--- tests/test_backends/test_qutip.py
+++ tests/test_backends/test_qutip.py
@@ -1,29 +1,39 @@
-from quantumion.hamiltonian.operator import *
+#%%
+import numpy as np
+
+from quantumion.hamiltonian.operator import PauliX, PauliY, PauliZ, PauliI, Annihilation, Creation
 from quantumion.hamiltonian.experiment import Experiment
 from backends.qsim.qutip import QutipBackend, Data
-
 from backends.base import Submission, Specification, Result
 
+#%% example of creating an operator
+op = np.pi * PauliX @ PauliX @ (Creation * Annihilation)
+print(op)
 
-op = 4.0 * PauliX @ PauliY @ PauliZ @ PauliI @ (Creation * Annihilation) @ (Annihilation * Creation)
-# print(op)
-
-
+#%% create an experiment to run/simulate
 ex = Experiment()
-ex.add(operator=PauliX @ PauliX)
-# ex.add(operator=PauliY @ PauliX @ Creation @ Annihilation)
-# print(ex)
+ex.add(operator=np.pi/4 * PauliX @ PauliX)
+print(ex)
 
-spec = Specification(n_shots=100, fock_trunc=4, observables={})
+#%% can serialize this to a JSON format...
+json_str = ex.model_dump()
+print(json_str)
 
-result = Result()
-data = Data()
-print(data)
+#%% ... and then easily parse back into data tree
+ex_parse = Experiment(**json_str)
+print(type(ex_parse))
+print(ex_parse)
 
-backend = QutipBackend()
+#%% need another object that stores keywords about the backend runtime parameters
+spec = Specification(n_shots=100, fock_trunc=4, observables={}, dt=0.1)
 
+#%% we package the program (i.e. the experiment) and the specification together as one object
 submission = Submission(program=ex, specification=spec)
 print(submission)
 
+#%% we can now run this using the Qutip simulator
+backend = QutipBackend()
 result = backend.run(submission)
 print(result)
+
+#%%
