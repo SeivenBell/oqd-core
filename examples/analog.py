63167a2b7171d20d098193251f3bcb22ddc4e2ed
Yi Hong Teoh <yhteoh@uwaterloo.ca>
2023-11-02 23:06:29 -0400

    Updated examples and tests

--- examples/analog.py
+++ examples/analog.py
@@ -1,21 +1,20 @@
 #%%
-import numpy as np
+from rich import print as pprint
 import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
-from quantumion.analog.operator import PauliX, PauliY, PauliZ
+from quantumion.analog.operator import PauliX, PauliY, PauliZ, PauliI
 from quantumion.analog.circuit import AnalogCircuit
 from quantumion.analog.gate import AnalogGate
-from backends.analog.qutip import QutipBackend, TaskArgsAnalog
-from backends.task import Task
+from backends.analog.qutip import QutipBackend
+from backends.task import Task, TaskArgsAnalog
 
-
-#%% example of creating an operator
-op = ((PauliY * PauliY) @ (PauliY * PauliX)) #@ (Creation * Annihilation)
 
 #%% create an experiment to run/simulate
 ex = AnalogCircuit()
 gate = AnalogGate(
-    duration=2.0,
+    duration=1.0,
     unitary=[np.pi / 4 * PauliX],
     dissipation=[]
 )
@@ -28,16 +27,23 @@ json_str = ex.model_dump()
 ex_parse = AnalogCircuit(**json_str)
 
 #%% need another object that stores keywords about the backend runtime parameters
-args = TaskArgsAnalog(n_shots=100, fock_trunc=4, observables={'z': PauliZ, 'x': PauliX, 'y': PauliY}, dt=0.01)
+args = TaskArgsAnalog(
+    n_shots=100,
+    fock_trunc=4,
+    observables={'z': PauliZ, 'x': PauliX, 'y': PauliY},
+    dt=0.1
+)
 
 #%% we package the program (i.e. the experiment) and the specification together as one object
 task = Task(program=ex, args=args)
-print(task)
+pprint(task)
 
 #%% we can now run this using the Qutip simulator
 backend = QutipBackend()
 result = backend.run(task)
-print(result.model_fields_set)
+
+#%%
+pprint(result)
 
 #%%
 fig, ax = plt.subplots()
@@ -47,3 +53,10 @@ ax.legend()
 plt.show()
 
 #%%
+rho = np.outer(result.state, np.conj(result.state))
+fig, axs = plt.subplots(1, 2)
+sns.heatmap(rho.real, ax=axs[0])
+sns.heatmap(rho.imag, ax=axs[1])
+for ax in axs:
+    ax.set_aspect('equal')
+plt.show()
