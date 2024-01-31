63dc72dfb6bb9fb1d149bd06ce0a575251b66fa7
yhteoh <yhteoh@uwaterloo.ca>
2024-01-31 16:38:13 -0500

    Added example protocol within quantumion.atomic.schedule

--- quantumion/atomic/schedule.py
+++ quantumion/atomic/schedule.py
@@ -137,7 +137,9 @@ class AtomicProgram(TypeReflectBaseModel):
 
 if __name__ == "__main__":
     try:
-        level1 = Level(
+        import numpy as np
+
+        dark_state = Level(
             principal=6,
             spin=0.5,
             orbital=0,
@@ -147,5 +149,78 @@ if __name__ == "__main__":
             spin_orbital_nuclear_magnetization=0,
             energy=0,
         )
+        bright_state = Level(
+            principal=6,
+            spin=0.5,
+            orbital=0,
+            nuclear=0.5,
+            spin_orbital=0.5,
+            spin_orbital_nuclear=1,
+            spin_orbital_nuclear_magnetization=0,
+            energy=12.6428e9,
+        )
+        excited_state = Level(
+            principal=6,
+            spin=0.5,
+            orbital=1,
+            nuclear=0.5,
+            spin_orbital=0.5,
+            spin_orbital_nuclear=0,
+            spin_orbital_nuclear_magnetization=0,
+            energy=811.29e12,
+        )
+        excited_state_2 = Level(
+            principal=6,
+            spin=0.5,
+            orbital=1,
+            nuclear=0.5,
+            spin_orbital=0.5,
+            spin_orbital_nuclear=1,
+            spin_orbital_nuclear_magnetization=0,
+            energy=811.29e12 + 2.105e9,
+        )
+        transition_1 = Transition(
+            level1=dark_state,
+            level2=excited_state_2,
+            multipole=Multipole(field="E", pole=1),
+            einstein_A=1,
+        )
+        transition_2 = Transition(
+            level1=bright_state,
+            level2=excited_state,
+            multipole=Multipole(field="E", pole=1),
+            einstein_A=1,
+        )
+
+        yb171plus = Ion(
+            mass=171,
+            charge=1,
+            levels=[dark_state, bright_state, excited_state, excited_state_2],
+            transitions=[transition_1, transition_2],
+        )
+
+        raman1 = Pulse(
+            ion=yb171plus,
+            transition=yb171plus.transitions[0],
+            rabi=1e6 * 2 * np.pi,
+            detuning=1e9,
+            polarization=[1, 0, 0],
+            wavevector=[0, 0, 1],
+        )
+        raman2 = Pulse(
+            ion=yb171plus,
+            transition=yb171plus.transitions[1],
+            rabi=1e6 * 2 * np.pi,
+            detuning=1e9,
+            polarization=[0, 0, 1],
+            wavevector=[0, 0, 1],
+        )
+
+        twophotonraman = Protocol(pulses=[raman1, raman2])
+
+        program = AtomicProgram(
+            statements=[Apply(protocol=twophotonraman, time=0.5e-6)]
+        )
+
     except ValidationError as e:
         print(e)
