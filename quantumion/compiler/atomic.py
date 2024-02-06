719030820a11872d0a69ae03db2865b3ac35fbae
yhteoh <yhteoh@uwaterloo.ca>
2024-02-06 17:14:03 -0500

    test: implemented test for a simple visitor for the atomic layer

--- quantumion/compiler/atomic.py
+++ quantumion/compiler/atomic.py
@@ -1,5 +1,5 @@
 from quantumion.compiler.visitor import Visitor, Transform
-from quantumion.atomic import Register
+from quantumion.atomic.schedule import Register
 
 ########################################################################################
 
