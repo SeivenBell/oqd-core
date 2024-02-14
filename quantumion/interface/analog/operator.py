ea781cd3fd1935e7e5ecf723648a2459bedaf1ec
yhteoh <yhteoh@uwaterloo.ca>
2024-02-14 12:47:17 -0500

    fix: Remove @ operation between MathExpr and Operator

--- quantumion/interface/analog/operator.py
+++ quantumion/interface/analog/operator.py
@@ -39,13 +39,13 @@ class Operator(TypeReflectBaseModel):
         return OpSub(op1=self, op2=other)
 
     def __matmul__(self, other):
-        if isinstance(other, Operator):
-            return OpKron(op1=self, op2=other)
-        else:
-            return OpScalarMul(op=self, expr=other)
+        return OpKron(op1=self, op2=other)
 
     def __mul__(self, other):
-        return OpMul(op1=self, op2=other)
+        if isinstance(other, Operator):
+            return OpMul(op1=self, op2=other)
+        else:
+            return OpScalarMul(op=self, expr=other)
 
     def __rmul__(self, other):
         other = MathExpr.cast(other)
