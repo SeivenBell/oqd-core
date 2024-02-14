ec21caeaf46827f33652c8df5561dae9ca551bbe
Yi Hong Teoh <yhteoh@uwaterloo.ca>
2024-02-14 07:30:38 -0500

    feat: Implemented GatherMathExpr canonicalization that gathers the MathExpr terms in an Operator

---
+++ quantumion/compiler/analog/canonicalize.py
@@ -0,0 +1,39 @@
+from typing import Union
+
+########################################################################################
+
+from quantumion.interface.analog import *
+
+from quantumion.compiler.analog.base import AnalogCircuitTransform
+
+########################################################################################
+
+
+class GatherMathExpr(AnalogCircuitTransform):
+    def visit_OpScalarMul(self, model: OpScalarMul):
+        if isinstance(model.op, OpScalarMul):
+            return model.expr * model.op.expr * self.visit(model.op.op)
+        return model.expr * self.visit(model.op)
+
+    def _visit_OpMulKron(self, model: Union[OpMul, OpKron]):
+        if isinstance(model.op1, OpScalarMul) and isinstance(model.op2, OpScalarMul):
+            return (
+                model.op1.expr
+                * model.op2.expr
+                * self.visit(model.__class__(op1=model.op1.op, op2=model.op2.op))
+            )
+        if isinstance(model.op1, OpScalarMul):
+            return model.op1.expr * self.visit(
+                model.__class__(op1=model.op1.op, op2=model.op2)
+            )
+        if isinstance(model.op2, OpScalarMul):
+            return model.op2.expr * self.visit(
+                model.__class__(op1=model.op1, op2=model.op2.op)
+            )
+        return model.__class__(op1=self.visit(model.op1), op2=self.visit(model.op2))
+
+    def visit_OpMul(self, model: OpMul):
+        return self._visit_OpMulKron(model)
+
+    def visit_OpKron(self, model: OpKron):
+        return self._visit_OpMulKron(model)
