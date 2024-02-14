e416a58f6965dead8d50e0ec73a4bebaef715dae
Yi Hong Teoh <yhteoh@uwaterloo.ca>
2024-02-14 17:57:43 -0500

    update: test_math script

--- tests/test_math/test_math.py
+++ tests/test_math/test_math.py
@@ -5,38 +5,74 @@ from rich import print as pprint
 from quantumion.interface.math import *
 from quantumion.interface.analog.operator import *
 
-from quantumion.compiler.math import PrintMathExpr, ReorderMathExpr, DeNestMathMul
-from quantumion.compiler.analog.base import PrintOperator
-from quantumion.compiler.analog.canonicalize import GatherMathExpr
-from quantumion.compiler.analog.verify import VerifyHilbertSpace
+from quantumion.compiler.math import *
+from quantumion.compiler.analog.base import *
+from quantumion.compiler.analog.canonicalize import *
+from quantumion.compiler.analog.verify import *
 
 ########################################################################################
 
 if __name__ == "__main__":
 
-    expr = MathStr(string="(1+2+4-5)*(2+1*(3+4*(3+5)*sin((a+2)*(6+d)**2)))")
-    pprint("{:^5}:".format(0), expr.accept(PrintMathExpr()))
-    for i in range(10):
-        expr = expr.accept(DeNestMathMul())
-        expr = expr.accept(ReorderMathExpr())
-        pprint("{:^5}:".format(i + 1), expr.accept(PrintMathExpr()))
+    # expr = MathStr(string="(1+2+4-5)*(2+1*(3+4*(3+5)*sin((a+2)*(6+d)**2)))")
+    # pprint("{:^5}:".format(0), expr.accept(PrintMathExpr()))
+    # for i in range(10):
+    #     expr = expr.accept(DeNestMathMul())
+    #     expr = expr.accept(ReorderMathExpr())
+    #     pprint("{:^5}:".format(i + 1), expr.accept(PrintMathExpr()))
 
     ########################################################################################
 
-    # expr = 1 * PauliX() @ PauliY() * MathStr(string="2") * MathStr(
-    #     string="sin(w*t + -k*x)"
-    # ) @ Annihilation() * (
-    #     PauliI() @ (PauliZ() * 1) @ Creation() * -MathStr(string="A")
-    # ) @ (
-    #     PauliI() @ (PauliZ() * 1) @ Creation() * MathStr(string="A")
-    # ) + (
-    #     PauliI() @ (PauliZ() * 1) @ Creation() * MathStr(string="-A")
-    # ) @ (
-    #     PauliI() @ (PauliZ() * 1) @ -Creation() * MathStr(string="+A")
-    # )
+    expr = 1 * PauliX() @ PauliY() * MathStr(string="2") * MathStr(
+        string="sin(w*t + -k*x)"
+    ) @ Annihilation() * (
+        PauliI() @ (PauliZ() * 1) @ Creation() * -MathStr(string="A")
+    ) @ (
+        PauliI() @ (PauliZ() * 1) @ Creation() * MathStr(string="A")
+    ) - (
+        PauliI() @ (PauliZ() * 1) @ Creation() * MathStr(string="-A")
+    ) @ (
+        PauliI() @ (PauliZ() * 1) @ -Creation() * MathStr(string="+A")
+    )
 
-    # for i in range(10):
-    #     expr = expr.accept(GatherMathExpr())
-    #     pprint("\n{:^5}:".format(i + 1), expr.accept(PrintOperator()))
+    pprint(
+        "\nHilbert Space  :\n\tPauli  : {}\n\tLadder : {}".format(
+            *expr.accept(VerifyHilbertSpace())
+        )
+    )
 
-    # pprint(expr.accept(VerifyHilbertSpace()))
+    i = 0
+    while True:
+        _expr = expr.accept(DistributeOp())
+        if _expr == expr:
+            break
+        i += 1
+        expr = _expr
+        pprint("\n{:^5}:".format(i), "\n" + expr.accept(VerbosePrintOperator()))
+
+    i = 0
+    while True:
+        _expr = expr.accept(GatherMathExpr())
+        if _expr == expr:
+            break
+        i += 1
+        expr = _expr
+        pprint("\n{:^5}:".format(i), "\n" + expr.accept(VerbosePrintOperator()))
+
+    i = 0
+    while True:
+        _expr = expr.accept(ProperKronOrder())
+        if _expr == expr:
+            break
+        i += 1
+        expr = _expr
+        pprint("\n{:^5}:".format(i), "\n" + expr.accept(VerbosePrintOperator()))
+
+    i = 0
+    while True:
+        _expr = expr.accept(SeparatePauliLadder())
+        if _expr == expr:
+            break
+        i += 1
+        expr = _expr
+        pprint("\n{:^5}:".format(i), "\n" + expr.accept(VerbosePrintOperator()))
