04ede108be00e51f01c6eb2f3b3f5d15a4518d14
yhteoh <yhteoh@uwaterloo.ca>
2024-02-14 15:28:10 -0500

    feat: Added ProperKronOrder, DeNestOpMulKron, SeparatePauliLadder transformers for canonicalization of an Operator

--- quantumion/compiler/analog/canonicalize.py
+++ quantumion/compiler/analog/canonicalize.py
@@ -43,3 +43,68 @@ class GatherMathExpr(AnalogCircuitTransformer):
 
     def _visit_OpAddSub(self, model: Union[OpAdd, OpSub]):
         return model.__class__(op1=self.visit(model.op1), op2=self.visit(model.op2))
+
+
+########################################################################################
+
+
+class ProperKronOrder(AnalogCircuitTransformer):
+    def visit_OpKron(self, model: OpKron):
+        if isinstance(model.op1, OpKron):
+            return OpKron(
+                op1=self.visit(model.op1.op1),
+                op2=OpKron(op1=self.visit(model.op1.op2), op2=self.visit(model.op2)),
+            )
+        return OpKron(op1=self.visit(model.op1), op2=self.visit(model.op2))
+
+
+class SeparatePauliLadder(AnalogCircuitTransformer):
+    def visit_OpKron(self, model: OpKron):
+        print(model.op1)
+        if isinstance(model.op1, Ladder):
+            if isinstance(model.op2, Pauli):
+                return OpKron(
+                    op1=model.op2,
+                    op2=model.op1,
+                )
+            if isinstance(model.op2, OpKron):
+                return OpKron(
+                    op1=model.op2.op1, op2=OpKron(op1=model.op1, op2=model.op2.op2)
+                )
+        return OpKron(op1=self.visit(model.op1), op2=self.visit(model.op2))
+
+
+class DeNestOpMulKron(AnalogCircuitTransformer):
+    def visit_OpMul(self, model: OpMul):
+        if isinstance(model.op1, (OpAdd, OpSub)):
+            return model.op1.__class__(
+                op1=self.visit(OpMul(op1=model.op1.op1, op2=model.op2)),
+                op2=self.visit(OpMul(op1=model.op1.op2, op2=model.op2)),
+            )
+        if isinstance(model.op2, (OpAdd, OpSub)):
+            return model.op2.__class__(
+                op1=self.visit(OpMul(op1=model.op1, op2=model.op2.op1)),
+                op2=self.visit(OpMul(op1=model.op1, op2=model.op2.op2)),
+            )
+        return OpMul(op1=self.visit(model.op1), op2=self.visit(model.op2))
+
+    def visit_OpKron(self, model: OpKron):
+        if isinstance(model.op1, (OpAdd, OpSub, OpMul)):
+            return model.op1.__class__(
+                op1=self.visit(OpKron(op1=model.op1.op1, op2=model.op2)),
+                op2=self.visit(OpKron(op1=model.op1.op2, op2=model.op2)),
+            )
+        if isinstance(model.op2, (OpAdd, OpSub, OpMul)):
+            return model.op2.__class__(
+                op1=self.visit(OpKron(op1=model.op1, op2=model.op2.op1)),
+                op2=self.visit(OpKron(op1=model.op1, op2=model.op2.op2)),
+            )
+        return OpKron(op1=self.visit(model.op1), op2=self.visit(model.op2))
+
+    def visit_OpScalarMul(self, model: OpScalarMul):
+        if isinstance(model.op, (OpAdd, OpSub)):
+            return model.op.__class__(
+                op1=OpScalarMul(op=self.visit(model.op.op1), expr=model.expr),
+                op2=OpScalarMul(op=self.visit(model.op.op2), expr=model.expr),
+            )
+        return OpScalarMul(op=self.visit(model.op), expr=model.expr)
