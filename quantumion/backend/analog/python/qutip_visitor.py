336f3584d645642ee17ebfa74bfdf4dc7f55d9f8
Sehmimul <sehmimul777@gmail.com>
2024-03-17 15:53:41 -0400

    minor: Added QutipCompiler and QutipBackendTransformer visitors

--- quantumion/backend/analog/python/qutip_visitor.py
+++ quantumion/backend/analog/python/qutip_visitor.py
@@ -1,7 +1,20 @@
 from quantumion.compiler.analog.base import AnalogInterfaceTransformer
 from quantumion.interface.analog.operator import *
 from quantumion.interface.analog.operations import *
+from rich import print as pprint
+
+__all__ = [
+    "QutipBackendTransformer",
+]
+class QutipCompiler(AnalogInterfaceTransformer):
+    """Transformer to run an experiment (maybe use visitor?)"""
+
+class QutipBackendTransformer(AnalogInterfaceTransformer):
+    """convert task to QutipObj without running (maybe use visitor?)"""
+
+    def visit_Evolve(self, model: Evolve):
+        pprint("Model is {}".format(model))
+        return model
 
 if __name__ == "__main__":
-    from rich import print as pprint
     X, Y, Z, I, A, C, J = PauliX(), PauliY(), PauliZ(), PauliI(), Annihilation(), Creation(), Identity()
