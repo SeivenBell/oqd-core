a7b6773694d42aeb00f402741fec1353e152e27d
Sehmimul <sehmimul777@gmail.com>
2024-03-18 15:51:09 -0400

    feat [minor]: qt_array removed from visitor with help from @yhteoh

--- quantumion/backend/analog/python/qutip_visitor.py
+++ quantumion/backend/analog/python/qutip_visitor.py
@@ -14,46 +14,24 @@ class QutipBackendTransformer(AnalogInterfaceTransformer):
     Basically compiles down to qutip object using transformers.
     """
     def __init__(self):
-        self.qt_array = []
         super().__init__()
 
     def visit_AnalogCircuit(self, model: AnalogCircuit):
         return self.visit(model.sequence)
     
     def visit_Evolve(self, model: Evolve):
-        self.qt_array = []
-        return self.visit(model.gate) 
+        return (self.visit(model.gate),model.duration)
 
     def visit_AnalogGate(self, model: AnalogGate):
         return self.visit(model.hamiltonian)
     
-    def visit_OperatorAdd(self, model: OperatorAdd):
-        self.visit(model.op2)
-        self.visit(model.op1)
-        return self.qt_array
-        # coefficient_op2 = model.op2.expr
-        # op_op2 = model.op2.op
-
-        # self.qt_array.append([op_op2, coefficient_op2])
-        # self.visit(model.op2)
-        # self.visit(model.op1)
-
-        # if not isinstance(model.op1, OperatorAdd):
-        #     coefficient_op1 = model.op1.expr
-        #     op_op1 = model.op1.op
-        #     self.qt_array.append([op_op1, coefficient_op1])
-        # else:
-        #     self.visit(model.op1)
-        #return self.qt_array 
-        # if isinstance(model.op1, OperatorAdd):
-        #     return self.visit(model.op1)
-        # else:
-        #     self.visit(model.op1)
-    
+    def visit_OperatorAdd(self, model: OperatorAdd):        
+        op = self.visit(model.op1)
+        op.append(self.visit(model.op2)[0])
+        return op
     
     def visit_OperatorScalarMul(self, model: OperatorScalarMul):
-        self.qt_array.append([model.op, model.expr])
-        return self.qt_array
+        return [(model.op, model.expr)]
 
 if __name__ == "__main__":
     X, Y, Z, I, A, C, J = PauliX(), PauliY(), PauliZ(), PauliI(), Annihilation(), Creation(), Identity()
