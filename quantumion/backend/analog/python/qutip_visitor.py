bf399b66fc48d8e80ff09a367b7644524bf13423
Sehmimul <sehmimul777@gmail.com>
2024-03-18 04:25:52 -0400

    feat [major]: Implemented Transformer for Qutip which transforms hamiltonians of sequence of analog gates to 2D arrays of the form [[Hamiltonian, coefficient], ...] following QuTip's format. Note that the Operators are still symbolic, so actual simulations cannot be run after this commit.

--- quantumion/backend/analog/python/qutip_visitor.py
+++ quantumion/backend/analog/python/qutip_visitor.py
@@ -6,15 +6,54 @@ from rich import print as pprint
 __all__ = [
     "QutipBackendTransformer",
 ]
-class QutipCompiler(AnalogInterfaceTransformer):
+class QutipExperiment(AnalogInterfaceTransformer):
     """Transformer to run an experiment (maybe use visitor?)"""
 
 class QutipBackendTransformer(AnalogInterfaceTransformer):
-    """convert task to QutipObj without running (maybe use visitor?)"""
+    """convert task to QutipObj without running (maybe use visitor?)
+    Basically compiles down to qutip object using transformers.
+    """
+    def __init__(self):
+        self.qt_array = []
+        super().__init__()
 
+    def visit_AnalogCircuit(self, model: AnalogCircuit):
+        return self.visit(model.sequence)
+    
     def visit_Evolve(self, model: Evolve):
-        pprint("Model is {}".format(model))
-        return model
+        self.qt_array = []
+        return self.visit(model.gate) 
+
+    def visit_AnalogGate(self, model: AnalogGate):
+        return self.visit(model.hamiltonian)
+    
+    def visit_OperatorAdd(self, model: OperatorAdd):
+        self.visit(model.op2)
+        self.visit(model.op1)
+        return self.qt_array
+        # coefficient_op2 = model.op2.expr
+        # op_op2 = model.op2.op
+
+        # self.qt_array.append([op_op2, coefficient_op2])
+        # self.visit(model.op2)
+        # self.visit(model.op1)
+
+        # if not isinstance(model.op1, OperatorAdd):
+        #     coefficient_op1 = model.op1.expr
+        #     op_op1 = model.op1.op
+        #     self.qt_array.append([op_op1, coefficient_op1])
+        # else:
+        #     self.visit(model.op1)
+        #return self.qt_array 
+        # if isinstance(model.op1, OperatorAdd):
+        #     return self.visit(model.op1)
+        # else:
+        #     self.visit(model.op1)
+    
+    
+    def visit_OperatorScalarMul(self, model: OperatorScalarMul):
+        self.qt_array.append([model.op, model.expr])
+        return self.qt_array
 
 if __name__ == "__main__":
     X, Y, Z, I, A, C, J = PauliX(), PauliY(), PauliZ(), PauliI(), Annihilation(), Creation(), Identity()
