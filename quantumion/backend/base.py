ed4d646a5eebe6b3fb8cea9645dde9e0bea4d633
yhteoh <yhteoh@uwaterloo.ca>
2024-04-04 15:10:57 -0400

    clean: quantumion.backend.base removed unused definitions

--- quantumion/backend/base.py
+++ quantumion/backend/base.py
@@ -1,6 +1,4 @@
 from abc import ABC, abstractmethod
-from typing import Optional
-from pydantic import BaseModel, model_validator
 
 ########################################################################################
 
@@ -10,29 +8,4 @@ class BackendBase(ABC):
     def run(self, task):
         pass
 
-    pass
-
-
-########################################################################################
-
-
-class TypeReflectBaseModel(BaseModel):
-    type_: Optional[str]
-
-    @model_validator(mode="before")
-    @classmethod
-    def reflect(cls, data):
-        if isinstance(data, BaseModel):
-            return data
-
-        if "class_" in data.keys():
-            if data["class_"] != cls.__name__:
-                raise ValueError('discrepency between "class_" field and model type')
-
-        data["class_"] = cls.__name__
-
-        return data
-
-
-class TypeReflectError(Exception):
     pass
