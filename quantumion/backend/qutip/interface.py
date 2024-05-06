cacf60d2ab20767773b58ecdf0115f32850e924c
Sehmimul <sehmimul777@gmail.com>
2024-05-06 12:24:03 -0400

    cleanup: removed field validator and instead store qt.Qobj and coefficient as Tuple

--- quantumion/backend/qutip/interface.py
+++ quantumion/backend/qutip/interface.py
@@ -1,8 +1,7 @@
 from quantumion.interface.base import VisitableBaseModel
 from quantumion.interface.math import MathExpr
 from quantumion.backend.metric import *
-from typing import List, Dict, Literal
-from pydantic import field_validator
+from typing import List, Dict, Literal, Tuple
 import qutip as qt
 from pydantic import ConfigDict
 from pydantic.types import NonNegativeInt
@@ -16,17 +15,7 @@ __all__ = [
 
 class QutipExpectation(VisitableBaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True)
-    operator: List[List]
-
-    @field_validator('operator')
-    @classmethod
-    def check_operator(cls, v):
-        for item in v:
-            if not isinstance(item, list) or len(item) != 2:
-                raise ValueError('Each item in the list must be a list with 2 elements')
-            if not isinstance(item[0], qt.Qobj) or not isinstance(item[1], MathExpr):
-                raise ValueError('Incorrect types in operator')
-        return v
+    operator: List[Tuple[qt.Qobj, MathExpr]]
     
 class TaskArgsQutip(VisitableBaseModel):
     layer: Literal["analog"] = "analog"
@@ -37,18 +26,9 @@ class TaskArgsQutip(VisitableBaseModel):
 
 class QutipOperation(VisitableBaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True)
-    hamiltonian: List[List]
+    hamiltonian: List[Tuple[qt.Qobj, MathExpr]]
     duration: float
 
-    @field_validator('hamiltonian')
-    @classmethod
-    def check_hamiltonian(cls, v):
-        for item in v:
-            if not isinstance(item, list) or len(item) != 2:
-                raise ValueError('Each item in the list must be a list with 2 elements')
-            if not isinstance(item[0], qt.Qobj) or not isinstance(item[1], MathExpr):
-                raise ValueError('Incorrect types in hamiltonian')
-        return v
     
 class QutipExperiment(VisitableBaseModel):
     instructions: list[QutipOperation]
