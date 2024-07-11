44768881ced610817eec34ed77cecdb55ac85211
Sehmimul <sehmimul777@gmail.com>
2024-07-12 01:44:43 +0600

    feat: implemented verification for distribution

--- quantumion/compilerv2/canonical_verification.py
+++ quantumion/compilerv2/canonical_verification.py
@@ -1,5 +1,5 @@
 from quantumion.compilerv2.base import PassBase
-from quantumion.compilerv2.generic import *
+from quantumion.compilerv2.rewriter import *
 from quantumion.compilerv2.rule import *
 from quantumion.compilerv2.walk import *
 from quantumion.interface.math import *
@@ -22,8 +22,77 @@ class CanVerPauliAlgebra(RewriteRule):
         else:
             return None
 
-    # def map_Operator(self, model):
-    #     pass            
+class CanVerGatherMathExpr(RewriteRule):
+    """Assuming that OperatorDistribute has already been fully ran"""
+
+    def map_OperatorMul(self, model: OperatorMul):
+        return self._mulkron(model)
+    
+    def map_OperatorKron(self, model: OperatorKron):
+        return self._mulkron(model)
+
+    def _mulkron(self, model: Union[OperatorMul, OperatorKron]):
+        if isinstance(model.op1, OperatorScalarMul) or isinstance(
+            model.op2, OperatorScalarMul
+        ):
+            raise CanonicalFormError("Incomplete Gather Math Expression")
+        return None
+
+    def map_OperatorScalarMul(self, model: OperatorScalarMul):
+        if isinstance(model.op, OperatorScalarMul):
+            raise CanonicalFormError(
+                "Incomplete scalar multiplications after GatherMathExpression"
+            )
+        return None
+
+class CanVerOperatorDistribute(RewriteRule):
+    def __init__(self):
+        super().__init__()
+        self.allowed_ops = Union[
+            OperatorTerminal,
+            Ladder,
+            OperatorMul,
+            OperatorScalarMul,
+            OperatorKron,
+        ]
+
+    def map_OperatorMul(self, model):
+        return self._OperatorMulKron(model)
+    
+
+    def map_OperatorKron(self, model):
+        return self._OperatorMulKron(model)
+
+
+    def _OperatorMulKron(self, model: (OperatorMul, OperatorKron)):
+        if (
+            isinstance(model, OperatorMul)
+            and isinstance(model.op1, OperatorKron)
+            and isinstance(model.op2, OperatorKron)
+        ):
+            raise CanonicalFormError(
+                "Incomplete Operator Distribution (multiplication of OperatorKron present)"
+            )
+        elif not (
+            isinstance(model.op1, self.allowed_ops)
+            and isinstance(model.op2, self.allowed_ops)
+        ):
+            raise CanonicalFormError("Incomplete Operator Distribution")
+        
+        pass
+       
+
+    def visit_OperatorScalarMul(self, model: OperatorScalarMul):
+        if not (isinstance(model.op, self.allowed_ops)):
+            raise CanonicalFormError(
+                "Scalar multiplication of operators not simplified fully"
+            )
+        pass
+
+    def visit_OperatorSub(self, model: OperatorSub):
+        if isinstance(model, OperatorSub):
+            raise CanonicalFormError("Subtraction of terms present")
+        pass
 class PruneIdentity(RewriteRule):
     """
     Assumptions: GatherMathExpr, OperatorDistribute, ProperOrder, GatherPauli, NormalOrder
@@ -227,29 +296,6 @@ class GatherPauli(RewriteRule):
                 )
         return None
     
-
-class CanVerGatherMathExpr(RewriteRule):
-    """Assuming that OperatorDistribute has already been fully ran"""
-
-    def map_OperatorMul(self, model: OperatorMul):
-        return self._mulkron(model)
-    
-    def map_OperatorKron(self, model: OperatorKron):
-        return self._mulkron(model)
-
-    def _mulkron(self, model: Union[OperatorMul, OperatorKron]):
-        if isinstance(model.op1, OperatorScalarMul) or isinstance(
-            model.op2, OperatorScalarMul
-        ):
-            raise CanonicalFormError("Incomplete Gather Math Expression")
-        return None
-
-    def map_OperatorScalarMul(self, model: OperatorScalarMul):
-        if isinstance(model.op, OperatorScalarMul):
-            raise CanonicalFormError(
-                "Incomplete scalar multiplications after GatherMathExpression"
-            )
-        return None
 
 class NormalOrder(RewriteRule):
     """
