359b466bb60fca74e7f560230490ed2fa3f667a0
Sehmimul <sehmimul777@gmail.com>
2024-08-02 22:23:35 +0600

    feat [important]: assign moved to rewrite

--- quantumion/compilerv2/analog/rewrite/assign.py
+++ quantumion/compilerv2/analog/rewrite/assign.py
@@ -1,24 +1,15 @@
 from typing import Any, Union
-
-########################################################################################
-
-from quantumion.interface.analog import *
-from quantumion.interface.math import MathExpr
+from quantumion.interface.analog import AnalogCircuit, AnalogGate
 from quantumion.compilerv2.rule import RewriteRule
-from quantumion.compilerv2.walk import Post, Pre
 from quantumion.compilerv2.analog.utils import get_canonical_hamiltonian_dim
 from quantumion.backend.metric import Expectation
-from rich import print as pprint
-
 
 
 class AssignAnalogIRDim(RewriteRule):
     def __init__(self):
         super().__init__()
         self.dim: Union[tuple, None] = None
-                
 
-    
     def map_AnalogCircuit(self, model: AnalogCircuit):
         model.n_qreg = self.dim[0]
         model.n_qmode = self.dim[1]
@@ -39,12 +30,3 @@ class VerifyAnalogIRDim(RewriteRule):
 
     def map_Expectation(self, model: Expectation):
         assert self._dim == get_canonical_hamiltonian_dim(model.operator)
-
-
-if __name__ == '__main__':
-    from quantumion.compiler.analog.base import *
-    X, Y, Z, I, A, C, LI = PauliX(), PauliY(), PauliZ(), PauliI(), Annihilation(), Creation(), Identity()
-    ac = AnalogCircuit()
-    ac.evolve(gate=AnalogGate(hamiltonian=1*(X@X) + 1*(Y@Y@(A*C))), duration=1)
-    ac.evolve(gate = AnalogGate(hamiltonian=1*(Y@Y@(A*C))), duration=1)
-    pprint(Post(AssignAnalogIRDim())(ac))
