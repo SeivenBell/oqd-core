5083c0b79a209ccedc71b67d3826dc8005917dfb
yhteoh <yhteoh@uwaterloo.ca>
2024-08-06 12:50:55 -0400

    Updated test_walk script

--- tests/test_compiler_infrastructure/test_walk.py
+++ tests/test_compiler_infrastructure/test_walk.py
@@ -12,6 +12,7 @@ from rich.console import Console
 ########################################################################################
 
 from quantumion.compilerv2 import *
+from quantumion.interface.base import VisitableBaseModel
 
 ########################################################################################
 
@@ -22,7 +23,10 @@ class PrintWalkOrder(RewriteRule):
         self.string = ""
 
     def generic_map(self, model):
-        self.string += f"\n{self.current_index}: {model}"
+        if isinstance(model, VisitableBaseModel):
+            self.string += f"\n{self.current_index}: {model.__class__.__name__}({', '.join(f'{model}'.split(' '))})"
+        else:
+            self.string += f"\n{self.current_index}: {model}"
         self.current_index += 1
         pass
 
@@ -35,8 +39,41 @@ class TestPreWalk(unittest.TestCase):
     def __init__(self, methodName: str = "runTest") -> None:
         super().__init__(methodName)
 
-    def test_pre(self):
-        "Simple Test of Pre Walk"
+    def test_pre_list(self):
+        "Test of Pre Walk on a list"
+        inp = ["a", "b"]
+
+        printer = Pre(PrintWalkOrder())
+
+        printer(inp)
+        assert printer.children[0].string == "\n0: ['a', 'b']\n1: a\n2: b"
+
+    def test_pre_dict(self):
+        "Test of Pre Walk on a dict"
+        inp = {"a": "a", "b": "b"}
+
+        printer = Pre(PrintWalkOrder())
+
+        printer(inp)
+        assert printer.children[0].string == "\n0: {'a': 'a', 'b': 'b'}\n1: a\n2: b"
+
+    def test_pre_VisitableBaseModel(self):
+        "Test of Pre Walk on a VisitableBaseModel"
+        inp = {"a": "a", "b": "b"}
+
+        class M(VisitableBaseModel):
+            a: str
+            b: str
+
+        inp = M(a="a", b="b")
+
+        printer = Pre(PrintWalkOrder())
+
+        printer(inp)
+        assert printer.children[0].string == "\n0: M(a='a', b='b')\n1: a\n2: b"
+
+    def test_pre_nested_list(self):
+        "Test of Pre Walk on a nested list"
         inp = ["a", ["b", "c"]]
 
         printer = Pre(PrintWalkOrder())
@@ -56,8 +93,41 @@ class TestPostWalk(unittest.TestCase):
     def __init__(self, methodName: str = "runTest") -> None:
         super().__init__(methodName)
 
-    def test_post(self):
-        "Simple Test of Post Walk"
+    def test_post_list(self):
+        "Test of Post Walk on a list"
+        inp = ["a", "b"]
+
+        printer = Post(PrintWalkOrder())
+
+        printer(inp)
+        assert printer.children[0].string == "\n0: a\n1: b\n2: ['a', 'b']"
+
+    def test_post_dict(self):
+        "Test of Post Walk on a dict"
+        inp = {"a": "a", "b": "b"}
+
+        printer = Post(PrintWalkOrder())
+
+        printer(inp)
+        assert printer.children[0].string == "\n0: a\n1: b\n2: {'a': 'a', 'b': 'b'}"
+
+    def test_post_VisitableBaseModel(self):
+        "Test of Post Walk on a VisitableBaseModel"
+        inp = {"a": "a", "b": "b"}
+
+        class M(VisitableBaseModel):
+            a: str
+            b: str
+
+        inp = M(a="a", b="b")
+
+        printer = Post(PrintWalkOrder())
+
+        printer(inp)
+        assert printer.children[0].string == "\n0: a\n1: b\n2: M(a='a', b='b')"
+
+    def test_post_nested_list(self):
+        "Test of Post Walk on a nested list"
         inp = ["a", ["b", "c"]]
 
         printer = Post(PrintWalkOrder())
